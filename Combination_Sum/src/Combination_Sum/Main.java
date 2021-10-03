c71897b31f47d5b14b0f453eb38ec5b9203f9542
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-03 15:43:00 -0400

    Update Main.java

--- Combination_Sum/src/Combination_Sum/Main.java
+++ Combination_Sum/src/Combination_Sum/Main.java
@@ -2,6 +2,11 @@ package Combination_Sum;
 
 public class Main {
     public static void main(String[] args){
-        
+        Combination c = new Combination();
+        int[] userArray = new int[]{1,2,3};
+        int target = 4;
+        int output;
+        output = c.combination_sum(userArray,target);
+        System.out.println(output);
     }
 }
